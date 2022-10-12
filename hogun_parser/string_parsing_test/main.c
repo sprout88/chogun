0de0afe4d0c59f0c38906d0293a835948f700576
a <a>
2022-10-12 14:43:31 +0900

    laptop auto commit by batch file

--- hogun_parser/string_parsing_test/main.c
+++ hogun_parser/string_parsing_test/main.c
@@ -12,8 +12,20 @@ int main()
 {
 	char Gstr1[]="$GNGGA,022427.00,3733.5415488,N,12659.6050902,E,4,12,0.50,49.027,M,18.475,M,1.0,0020*5E";
 	
+	char* tok[100];
 	
-	printf("%d\n",a[0]);
+	int i=0;
+	tok[i] = strtok(Gstr1,",");
+	while(tok[i]!=NULL){
+		tok[i]=strtok(NULL,",");
+		i++;
+	}
+	int j=0;
+	for(j=0;j<=100;j++){
+		printf("%s\n",tok[j]);
+	}
+	
+	
 	
 		
     return 0;
