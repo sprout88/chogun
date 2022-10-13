4860783882f540579dadb4375f2b29f7db6b6b9c
88sprout <88sprout@gmail.com>
2022-10-14 07:54:00 +0900

    laptop auto commit by batch file

--- hogun_parser_structure/subStringByIndex.c
+++ hogun_parser_structure/subStringByIndex.c
@@ -2,33 +2,27 @@
 #include <stdio.h>
 #include <string.h>
 
-/*
-char* subStringByIndex(char* str,int startIndex,int endIndex){
-	char* startStr = 
-	
-	
-	return 0;
-}
-*/
 
-int main(void){
-	char originStr[] = "Hello World";
-	char subStr[100]="";
+char* subStringByIndex(char* originStr,int startIndex,int endIndex){
 	
-	puts(originStr);
-
-	int startIndex = 3;
-	int endIndex = 6;
-	
+	static char subStr[1000]="";
 	int i=0;
-	int j=0;
+
 	for(i=0;i<strlen(originStr);i++){
 		if(i>=startIndex&&i<=endIndex){
-			subStr[i]=originStr[i];
+				subStr[i-startIndex]=originStr[i];	
 			}
 		}
-		
-		puts(subStr);
+	return subStr;
+}
+
+
+int main(void){
+	
+	char* originStr = "lets play with john";
+	char* subStr = subStringByIndex(originStr,15,19);
+	puts(subStr);
+
 
 }
 
